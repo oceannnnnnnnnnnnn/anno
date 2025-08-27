762e46cffe4d58cc5baaa6a3d1d39cebdb3365d1
wm5410 <williammalone7654321@gmail.com>
2025-08-27 14:30:04 -0700

    chenge

--- app.js
+++ app.js
@@ -1,32 +1,3 @@
-// const http = require('http')
-// const fs = require('fs')
-// const port = process.env.PORT || 3000; 
-
-// const server = http.createServer(function(req, res){
-//     res.writeHead(200, {'Content-Type':'text/html'})
-//     fs.readFile('index.html', function(error, data){
-//         if (error){
-//             res.writeHead(404)
-//             res.write("File not found")
-//         }else{
-//             res.write(data)
-//         }
-//         res.end()
-//     })
-// })
-
-// server.listen(port, function(error) {
-//     if (error){
-//         console.log('Something went wrong', error)
-//     }else{
-//         console.log('Server is listening on Port: ' + port)
-//     }
-// })
-
-
-
-
-
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
